c6e4795be3c6851718128d6de710f18ca84cbe21
Pratik Bagmare <pratikbagmare@gmail.com>
2024-08-16 16:23:00 -0700

    removed default import error

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import {store} from './redux/store.ts'
+import App from './App.tsx'
+import { store } from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
